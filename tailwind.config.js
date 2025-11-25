4fabb6fd4f727b167f3025b08a9b0c50a3eab007
MarcoBertaccini <marco.bertaccini9@gmail.com>
2025-11-25 23:21:23 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
     extend: {
       colors: {
         'relab-blue': '#003A5C',
@@ -9,10 +22,10 @@ export default {
         'relab-gray': '#F4F4F4',
       },
       fontFamily: {
-        'sans': ['Nunito', 'sans-serif'],
+        sans: ['Nunito', 'sans-serif'],  // Nunito = font di default
       },
       borderRadius: {
-        'relab': '20px',
+        relab: '20px',
         'relab-lg': '28px',
       },
     },
